7a5ffad64c523e5b27093c900073dc07b9fbe921
Joao Ferreira <joaojalf@gmail.com>
2024-11-30 14:39:26 +0100

    added avatar component

---
+++ src/app/shared/ui/avatar.component.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+
+@Component({
+  selector: 'app-avatar',
+  imports: [NgOptimizedImage],
+  template: `
+    <img [ngSrc]="avatar" alt="Avatar" class="avatar" width="25" height="25" />
+  `,
+})
+export class AvatarComponent {
+  avatar = input<string>('avatar.png');
+}
