d621b87e44ce08fab6a4a69bede560e353e65ef6
Joao Ferreira <joaojalf@gmail.com>
2025-02-18 18:56:24 +0100

    created work-experience component, moved contact-me component to modules and updated route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { WelcomeComponent } from './modules/home/components/welcome/welcome.component';
-import { ContactMeComponent } from './modules/home/components/contact-me/contact-me.component';
+import { ContactMeComponent } from './modules/contact-me/contact-me.component';
+import { WorkExperienceComponent } from './modules/work-experience/work-experience.component';
 
 export const routes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'contact-me', component: ContactMeComponent },
+  { path: 'work-experience', component: WorkExperienceComponent },
 ];
